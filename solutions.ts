75a32d6f1c12f2e7d6df971a3bdf005877d6c1e5
Subroto Chanda Shuvo <subrotochandashuvo@gmail.com>
2026-05-21 00:51:31 +0600

    added Problem 5 , Problem 6 and Problem 7 implementations.

--- solutions.ts
+++ solutions.ts
@@ -1,36 +1,92 @@
 // Problem 1
 function filterEvenNumbers(numbers: number[]): number[] {
-    return numbers.filter(num => num % 2 === 0);
+  return numbers.filter((num) => num % 2 === 0);
 }
 console.log(filterEvenNumbers([1, 2, 3, 4, 5, 6]));
-
+// -------------------------END-------------------------
 
 // Problem 2
 function reverseString(str: string): string {
-    return str.split('').reverse().join('');
+  return str.split("").reverse().join("");
 }
 console.log(reverseString("typescript"));
-
+// -------------------------END-------------------------
 
 // Problem 3
 type StringOrNumber = string | number;
 function checkType(input: StringOrNumber): string {
-    if (typeof input === 'string') {
-        return "String";
-    } else {
-        return "Number";
-    }
+  if (typeof input === "string") {
+    return "String";
+  } else {
+    return "Number";
+  }
 }
 console.log(checkType("Hello"));
 console.log(checkType(42));
 // console.log(checkType(true)); // wrong type
-
+// -------------------------END-------------------------
 
 // Problem 4
 function getProperty<T, K extends keyof T>(obj: T, key: K): T[K] {
-     return obj[key];
+  return obj[key];
 }
 const user = { id: 1, name: "John Doe", age: 21 };
 console.log(getProperty(user, "name"));
+// -------------------------END-------------------------
 
 // Problem 5
+interface Book {
+  title: string;
+  author: string;
+  publishedYear: number;
+}
+
+function toggleReadStatus(book: Book): Book & { isRead: boolean } {
+  return {
+    ...book,
+    isRead: true,
+  };
+}
+
+const myBook = {
+  title: "TypeScript Guide",
+  author: "Jane Doe",
+  publishedYear: 2024,
+};
+console.log(toggleReadStatus(myBook));
+// -------------------------END-------------------------
+
+// Problem 6
+class Person {
+  name: string;
+  age: number;
+
+  constructor(name: string, age: number) {
+    this.name = name;
+    this.age = age;
+  }
+}
+
+const Student = class extends Person {
+  grade: string;
+
+  constructor(name: string, age: number, grade: string) {
+    super(name, age);
+    this.grade = grade;
+  }
+
+  getDetails(): string {
+    return `Name: ${this.name}, Age: ${this.age}, Grade: ${this.grade}`;
+  }
+};
+
+const student = new Student("Alice", 20, "A");
+console.log(student.getDetails());
+// -------------------------END-------------------------
+
+// Problem 7
+function getIntersection(arr1: number[], arr2: number[]): number[] {
+  return arr1.filter((num) => arr2.includes(num));
+}
+console.log(getIntersection([1, 2, 3, 4, 5], [3, 4, 5, 6, 7]));
+// -------------------------END-------------------------
