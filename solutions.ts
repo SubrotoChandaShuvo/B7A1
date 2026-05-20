d787fd4626993d5add6dcf29572d7a15424ad621
Subroto Chanda Shuvo <subrotochandashuvo@gmail.com>
2026-05-20 16:54:27 +0600

    answer of Problem1 and Problem 2

--- solutions.ts
+++ solutions.ts
@@ -1,1 +1,12 @@
 // Problem 1
+function filterEvenNumbers(numbers: number[]): number[] {
+    return numbers.filter(num => num % 2 === 0);
+}
+console.log(filterEvenNumbers([1, 2, 3, 4, 5, 6]));
+
+
+// Problem 2
+function reverseString(str: string): string {
+    return str.split('').reverse().join('');
+}
+console.log(reverseString("typescript"));
